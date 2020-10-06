c9ca4eaf030debfdb5bb3ebdb580c67caa249bbe
nishchay271095 <nishchay271095@gmail.com>
2020-10-06 08:27:09 +0530

    Add default timestamp

--- src/routes/timestamp.js
+++ src/routes/timestamp.js
@@ -3,17 +3,26 @@ const moment = require('moment');
 
 const router = express.Router();
 
+router.get('/', (req,res)=>{
+    let outputFormat;
+    let date;
+    date = new Date();
+    outputFormat = { unix: date.getTime(), utc: date.toUTCString()};
+    res.json(outputFormat);
+})
+
+
 router.get('/:date_string', (req,res)=>{
     const date_string = req.params.date_string;
     let outputFormat;
     let date;
     if(isNaN(date_string))
     {
-        const isValidDate = moment(date_string,"YYYY-MM-DD",true).isValid();
+        const isValidDate =  moment(date_string,"YYYY-M-D",true).isValid();
         if(isValidDate)
         {
             date = new Date(date_string);
-            outputFormat = {"unix": date.getTime(), "utc": date.toUTCString()};
+            outputFormat = { unix: date.getTime(), utc: date.toUTCString()};
         }
         else
         {
@@ -24,7 +33,7 @@ router.get('/:date_string', (req,res)=>{
     {
         const num = parseInt(date_string);
         date = new Date(num);
-        outputFormat = {"unix": date.getTime(), "utc": date.toUTCString()};
+        outputFormat = { unix: date.getTime(), utc: date.toUTCString()};
     }
     res.json(outputFormat);
 });
